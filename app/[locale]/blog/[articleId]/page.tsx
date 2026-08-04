570aceebe8041c76246deb0749e91be422b79dc0
greest <moazahmed.ghania@gmail.com>
2026-08-04 14:26:15 +0300

    fix: use force-dynamic on article page (revalidate conflicts with redirect)

--- app/[locale]/blog/[articleId]/page.tsx
+++ app/[locale]/blog/[articleId]/page.tsx
@@ -1,4 +1,4 @@
-export const revalidate = 3600; // regenerate every hour
+export const dynamic = 'force-dynamic';
 
 import { notFound, redirect } from 'next/navigation'
 import type { Metadata } from 'next'
